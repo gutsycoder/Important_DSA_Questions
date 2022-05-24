aa1b3d7588b50158ea3fbaf193a8b024aaaf5935
gutsycoder <suyashsincever13@gmail.com>
2022-05-25 00:41:46 +0530

    Added solution of MinimumPassesMatrix

--- MinimumPassesMatrix.py
+++ MinimumPassesMatrix.py
@@ -27,3 +27,63 @@
 # [1, -2, -5, -1, -3],
 # [3, 0, 0, -4, -1]]
 # Sample Output  3
+
+from collections import deque
+
+def minimumPassesOfMatrix(matrix):
+    passes=convertNegatives(matrix)
+    return passes -1 if not containsNegative(matrix) else -1
+
+
+def convertNegatives(matrix):
+    queue=deque(getAllPositivePositions(matrix))
+    passes=0
+    while queue:
+        currentSize=len(queue)
+        while currentSize>0:
+            currentRow,currentCol=queue.popleft()
+            neighbors=getNeighbors(currentRow,currentCol,matrix)
+            for neighbor in neighbors:
+                row,col=neighbor
+                value=matrix[row][col]
+                if value<0:
+                    matrix[row][col]*=-1
+                    queue.append([row,col])
+            currentSize-=1
+        passes+=1
+
+    return passes
+def getAllPositivePositions(matrix):
+    positivePositions=[]
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            if matrix[row][col]>0:
+                positivePositions.append([row,col])
+
+    return positivePositions
+
+def getNeighbors(row,col,matrix):
+    neighbors=[]
+    if row>0:
+        neighbors.append([row-1,col])
+    if row<len(matrix)-1:
+        neighbors.append([row+1,col])
+    if col>0:
+        neighbors.append([row,col-1])
+    if col<len(matrix[0])-1:
+        neighbors.append([row,col+1])
+    return neighbors
+def containsNegative(matrix):
+    for row in matrix:
+        for value in row:
+            if value<0:
+                return True
+    return False
+
+if __name__=="__main__":
+    matrix = [
+    [0, 1, 3, 2, 0],
+    [1, -2, -5, -1, -3],
+    [3, 0, 0, -4, -1]]
+
+    print(minimumPassesOfMatrix(matrix))
