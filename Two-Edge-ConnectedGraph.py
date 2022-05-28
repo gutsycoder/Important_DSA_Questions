c1db35d409244eaf3230b4b725af73013578feef
gutsycoder <suyashsincever13@gmail.com>
2022-05-28 15:37:03 +0530

    Added 2-Edge Connected Graph using Tarjan's Algorithm

--- Two-Edge-ConnectedGraph.py
+++ Two-Edge-ConnectedGraph.py
@@ -18,7 +18,64 @@
 # [0, 1, 3],
 # [2, 4, 5],
 # [3, 5],
-# [0, 3, 4],
+# [0, 3, 4]]
 # Sample Output
 # true
-#It can be solved using an Algorithm similar to Tarjan's Algorithm 
+#It can be solved using an Algorithm similar to Tarjan's Algorithm
+
+def twoEdgeConnectedGraph(edges):
+    if len(edges)==0:
+        return True
+
+    arrivalTimes=[-1]*len(edges)
+    startVertex=0
+
+    if getMinimumArrivalTimeOfAncestors(startVertex,-1,0,arrivalTimes,edges)==-1:
+        return False
+
+    return areAllVerticesVisited(arrivalTimes)
+
+def areAllVerticesVisited(arrivalTimes):
+    for time in arrivalTimes:
+        if time==-1:
+            return False
+
+    return True
+
+def getMinimumArrivalTimeOfAncestors(currentVertex,parent,currentTime,arrivalTimes,edges):
+    arrivalTimes[currentVertex]=currentTime
+    minimumArrivalTime=currentTime
+
+    for destination in edges[currentVertex]:
+        if arrivalTimes[destination]==-1:
+            minimumArrivalTime=min(minimumArrivalTime,getMinimumArrivalTimeOfAncestors(destination,currentVertex,currentTime+1,arrivalTimes,edges))
+        elif destination!=parent:
+            minimumArrivalTime=min(minimumArrivalTime,arrivalTimes[destination])
+    if minimumArrivalTime==currentTime and parent !=-1:
+        return -1
+    return minimumArrivalTime
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    edges = [
+    [1, 2, 5],
+    [0, 2],
+    [0, 1, 3],
+    [2, 4, 5],
+    [3, 5],
+    [0, 3, 4]]
+    print(twoEdgeConnectedGraph(edges))
