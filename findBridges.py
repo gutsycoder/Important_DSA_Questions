f1cb0be32f36ad44ac6cba2c9d6fa38888e0c1fd
gutsycoder <suyashsincever13@gmail.com>
2022-05-29 23:06:26 +0530

    Had to modify the code .Just written the crux

--- findBridges.py
+++ findBridges.py
@@ -1,1 +1,87 @@
 #Find Bridges In the Graph
+"""
+    Time Complexity : O(V + E)
+    Space Complexoty : O(V)
+
+    Where V is the number of vertices and E is the number of edges in the graph.
+"""
+
+time = 0
+
+
+def dfs(graph, allBridges, visited, ins, low, parent, currentVertex):
+
+    # Mark currentVertex as visited.
+    visited[currentVertex] = True
+
+    # Initialize discovery time and low value
+    global time
+    time += 1
+    low[currentVertex] = ins[currentVertex] = time
+
+    # Iterate all the adjacent vertices to currentVertex node.
+    for child in graph[currentVertex]:
+        # child is the current adjacent vertex of currentVertex
+
+        # If child is not visited yet then recur for it.
+        if not visited[child]:
+
+            dfs(graph, allBridges, visited, ins, low, currentVertex, child)
+
+            # Check if the subtree rooted with child has a connection to one of the
+            # ancestors of currentVertex
+            low[currentVertex] = min(low[child], low[currentVertex])
+
+            if low[child] > ins[currentVertex]:
+                # Add an edge(currentVertex-child) to answer.
+                edge = [0] * 2
+                edge[0] = currentVertex
+                edge[1] = child
+                allBridges.append(edge)
+
+        elif child != parent:
+            # If adjacent vertex is alreedy visited before, then update the low of currentVertex.
+            low[currentVertex] = min(low[currentVertex], ins[child])
+
+
+# Function to add edge to graph.
+def addedge(graph, a, b):
+    graph[a].append(b)
+    graph[b].append(a)
+
+
+def findBridges(edges, v, e):
+    graph = [[] for i in range(v)]
+
+    # Adding edges in the graph.
+    for ei in range(e):
+
+        a = edges[ei][0]
+        b = edges[ei][1]
+
+        addedge(graph, a, b)
+
+    # Initialise time to 0
+    global time
+    time = 0
+
+    # It keep track of visited vertices.
+    visited = [0] * v
+
+    # It stores discovery time of every vertex
+    ins = [0] * v
+
+    # For every vertex it stores, the discovery time of the earliest discovered
+    # vertex to which or any of the vertices in the subtree rooted at is
+    # having a back edge.
+    low = [0] * v
+
+    allBridges = []
+
+    # Call the recursive helper function to find bridges in DFS tree rooted with
+    # vertex i.
+    for i in range(v):
+        if not visited[i]:
+            dfs(graph, allBridges, visited, ins, low, -1, i)
+
+    return allBridges
