4832b08eab09257c6de31e6e7fb486cc7089e05b
gutsycoder <suyashsincever13@gmail.com>
2022-05-31 20:10:20 +0530

    Added Permutations Questions

---
+++ Permutations.py
@@ -0,0 +1,5 @@
+#PERMUTATIONS
+#Write a functionthat takes array of unique integers and returns an array of all permutations of  those integers in the array
+#in no particular order
+#Sample Input [1,2,3]
+#Sample Output [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
