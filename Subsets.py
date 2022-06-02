9dd8c2145a8019dc3f86b63e570f4f51c57b6ad8
gutsycoder <suyashsincever13@gmail.com>
2022-06-02 16:06:11 +0530

    Added subset question

---
+++ Subsets.py
@@ -0,0 +1,7 @@
+#POWERSETS / SUBSETS
+#THE POWERSET P(X) OF A SET X IS THE SET OF ALL SUBSETS OF X. WRITE A FUNCTION THAT TAKES ARRAY OF UNIQUE INTEGERS
+#AND RETURN IT'S POWERSET
+# Sample Input
+# array = [1, 2, 3]
+# Sample Output
+# [[], [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3]]
