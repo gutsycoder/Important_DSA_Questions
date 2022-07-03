7eaf699714f0e9e0c1b3ecb53c8c836bf049c9b3
gutsycoder <suyashsincever13@gmail.com>
2022-07-03 10:01:51 +0530

    Shifted Binary Search Question

---
+++ shiftedBinary_search.py
@@ -0,0 +1,5 @@
+#Shifted Binary Search
+# Write a function that takes in a sorted array of distinct integers as well as a target integer.
+#The caveat is that the integers in the array have been shifted by some amount; in other words, they've been moved to the left or to the right by one or more positions.
+#For example, [1, 2, 3, 4] might have turned into [3, 4, 1, 2].
+# The function should use a variation of the Binary Search algorithm to determine if the target integer is contained in the array and should return its index if it is, otherwise -1.
